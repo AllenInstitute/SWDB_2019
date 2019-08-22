2eba41e95f6a2bb3aadd0819d566cb5dd59e5d98
Forrest Collman <forrest.collman@gmail.com>
2019-08-22 13:45:45 -0700

    adding bin bash shebang

--- scripts/em_conda_install.sh
+++ scripts/em_conda_install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # activate your conda environment of choice first
 conda install -y -c conda-forge shapely itkwidgets k3d pykdtree pyembree
 pip install -r https://gist.githubusercontent.com/fcollman/45285126b53cf03f01b2c2ff33705f00/raw/gistfile1.txt
