5afc8fcec0edfa43aa8dda98199dc9e1ec9ed1b5
Forrest Collman <forrest.collman@gmail.com>
2019-08-22 13:48:36 -0700

    fixing marshmallow version

--- scripts/em_conda_install.sh
+++ scripts/em_conda_install.sh
@@ -2,3 +2,4 @@
 # activate your conda environment of choice first
 conda install -y -c conda-forge shapely itkwidgets k3d pykdtree pyembree
 pip install -r https://gist.githubusercontent.com/fcollman/45285126b53cf03f01b2c2ff33705f00/raw/gistfile1.txt
+pip install marshmallow==3.0.0rc6
